85c85ef239dc43d671e28673756eb1227dc90d64
Ankit Arora <arora.ankit7@gmail.com>
2025-09-01 11:44:10 +0530

    Added exit conditions

--- sockets/tcp-simple-2/client.py
+++ sockets/tcp-simple-2/client.py
@@ -6,4 +6,10 @@ sock.connect(("127.0.0.1", 12345))
 
 while True:
     data = input()
+    if data == "exit":
+        break
+
     sock.send(data.encode())
+
+
+sock.close()
--- sockets/tcp-simple-2/server.py
+++ sockets/tcp-simple-2/server.py
@@ -16,6 +16,8 @@ print(f"Got connection from client: {addr}")
 
 while True:
     data = conn.recv(1024)
+    if not data:
+        break
     print(f"Data from client = {data.decode()}")
 
 conn.close()
