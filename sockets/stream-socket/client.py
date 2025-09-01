c0a61aa378f6003f9659d7b5aba7ce5e615bff84
Ankit Arora <arora.ankit7@gmail.com>
2025-09-01 12:40:41 +0530

    Added socket programming code

---
+++ sockets/stream-socket/client.py
@@ -0,0 +1,9 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect(("127.0.0.1", 12345))
+
+sock.send("hello 1".encode())
+sock.send("hello 2".encode())
+sock.send("hello 3".encode())
--- sockets/udp-simple/server.py
+++ sockets/udp-simple/server.py
@@ -6,5 +6,5 @@ interface = "127.0.0.1"
 port = 12345
 sock.bind((interface, port))
 
-data = sock.recv(1024)
-print(f"data from client = {data.decode()}")
+data, addr = sock.recvfrom(1024)
+print(f"data from client = {data.decode()} from addr = {addr}")
